893a0be0a5f38c157aa7d353723facdc287a5201
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-12-13 20:09:25 +0530

    Fixing Cart Problems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { sendCartData, fetchCartData } from "./Store/cartFunctions";
 import { useSelector, useDispatch } from "react-redux";
 import { Fragment } from "react";
 import { useEffect } from "react";
-import { uiActions } from "./Store/uiSlice";
 import Notification from "./components/UI/Notification";
 
 let initialVal = true;
@@ -14,47 +14,19 @@ function App() {
     const notification = useSelector((state) => state.ui.notification);
 
     useEffect(() => {
-        const sendCartData = async () => {
-            dispatch(
-                uiActions.showNotification({
-                    status: "pending",
-                    title: "sending",
-                    message: "Sending data....",
-                })
-            );
-            const response = await fetch(
-                "https://cart-redux-1111-default-rtdb.firebaseio.com/cart.json",
-                {
-                    method: "PUT",
-                    body: JSON.stringify(cart),
-                }
-            );
-            if (!response.ok) {
-                throw new Error("Sending cart data failed..");
-            }
-            dispatch(
-                uiActions.showNotification({
-                    status: "success",
-                    title: "success",
-                    message: "Cart data sent successfuly",
-                })
-            );
-        };
+        dispatch(fetchCartData());
+    }, [dispatch]);
 
+    useEffect(() => {
         if (initialVal) {
             initialVal = false;
             return;
         }
-        sendCartData().catch((err) => {
-            dispatch(
-                uiActions.showNotification({
-                    status: "error",
-                    title: "error",
-                    message: "Error in sending data",
-                })
-            );
-        });
+        if (cart.isChanged) {
+            dispatch(sendCartData(cart));
+        }
     }, [cart, dispatch]);
+
     return (
         <Fragment>
             {notification && (
